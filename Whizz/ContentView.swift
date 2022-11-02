e684491867ab88e685f932c914c2b92fbb1dbd4a
Shaivik Dhavjekar <shaivik.dhavjekar@srijan.net>
2022-11-02 13:56:51 +0530

    Project Setup

---
+++ Whizz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whizz
+//
+//  Created by Jitesh Acharya on 02/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whizz/WhizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhizzApp.swift
+//  Whizz
+//
+//  Created by Jitesh Acharya on 02/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WhizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
