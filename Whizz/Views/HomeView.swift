2e4268830eed77e04ea4a5ede947c5a254945608
Shaivik Dhavjekar <shaivik.dhavjekar@srijan.net>
2022-11-10 19:28:16 +0530

    Making HomeView Resizable

--- Whizz/Views/HomeView.swift
+++ Whizz/Views/HomeView.swift
@@ -24,104 +24,100 @@ struct HomeView: View {
     @State var isSearchToViewShowing: Bool = false
     
     var body: some View {
-
-        ZStack(alignment: .top) {
-            
-            Color.black.edgesIgnoringSafeArea(.all)
-            
-            GifImageView("AirplaneGif")
-                .frame(width:UIScreen.main.bounds.width*0.8, height: 233)
-                .allowsHitTesting(false)
-            
-            VStack{
-               
-                Spacer().frame(height: 180)
-                            Text("Whizz")
-                                .fontWeight(.thin)
-                                .foregroundColor(Color(UIColor.ComponentColor))
-                                .font(.system(size: 80))
-
-                            GroupBox{
-                                HStack{
-                                    
-                                    Button {
-                                        isSearchFromViewShowing.toggle()
-                                    } label: {
-                                        if from == "" {
-                                            Text("FROM")
-                                        } else {
-                                            VStack(spacing: 7) {
-                                                Text("FROM").font(.system(.caption))
-                                                Text(from).font(.system(size: 20))
-                                            }
-                                        }
-                                    }
-                                    .frame(width: 150, height:70.0)
-                                    .foregroundColor(.black).opacity(0.4)
-                                    
-//                                    TextField("from", text: $origin)
-//                                        .frame(height: 60.0)
-
-
-                                    Rectangle()
-                                        .fill(.black)
-                                        .frame(width:1,height:30)
-                                        .opacity(0.4)
-                                    
-                                    Button {
-                                        isSearchToViewShowing.toggle()
-                                    } label: {
-                                        if to == "" {
-                                            Text("TO")
-                                        } else {
-                                            VStack(spacing: 7) {
-                                                Text("TO").font(.system(.caption))
-                                                Text(to).font(.system(size: 20))
-                                            }
-                                        }
-                                    }
-                                    .frame(width: 150, height: 70.0)
-                                    .foregroundColor(.black).opacity(0.4)
-
-//                                    TextField("Where to?", text: $target)
-//                                        .frame(height: 60.0)
-
-                                }
-                                DateView(date:$departure,dateString:$departureString)
-                                    .frame(height:70.0)
-                                    .foregroundColor(.black).opacity(0.4)
-    
-
-                            }label: {
-                                Label("Search flight", systemImage: "airplane.arrival")
-                                    .opacity(0.6)
-                            }
-                            .groupBoxStyle(.created)
-
-        //                    Spacer().frame(height: 20)
-
-                            Button{
-
+        
+        GeometryReader { geometry in
+            ZStack(alignment: .top) {
+                
+                Color.black.edgesIgnoringSafeArea(.all)
+                
+                GifImageView("AirplaneGif")
+                    .frame(width:geometry.size.width*0.8, height: geometry.size.width*0.59)
+                    .allowsHitTesting(false)
+                
+                VStack{
+                    
+                    Spacer().frame(height: 180)
+                    
+                    Text("Whizz")
+                        .fontWeight(.thin)
+                        .foregroundColor(Color(UIColor.ComponentColor))
+                        .font(.system(size: 80))
+                    
+                    GroupBox{
+                        HStack{
+                            
+                            Button {
+                                isSearchFromViewShowing.toggle()
                             } label: {
-                                Text("SEARCH")
-                                    .frame(height: 51)
-                                    .foregroundColor(.black)
-                                    .frame(width:UIScreen.main.bounds.width*0.8)
-                                    .background(Color(UIColor.ComponentColor))
-                                    .cornerRadius(8.0)
+                                if from == "" {
+                                    Text("FROM")
+                                } else {
+                                    VStack(spacing: 7) {
+                                        Text("FROM").font(.system(.caption))
+                                        Text(from).font(.system(size: 20))
+                                    }
+                                }
                             }
-                            .padding(.top, 30)
-
+                            .frame(width: 150, height:70.0)
+                            .foregroundColor(.black).opacity(0.4)
+                            
+                            Rectangle()
+                                .fill(.black)
+                                .frame(width:1,height:30)
+                                .opacity(0.4)
+                            
+                            Button {
+                                isSearchToViewShowing.toggle()
+                            } label: {
+                                if to == "" {
+                                    Text("TO")
+                                } else {
+                                    VStack(spacing: 7) {
+                                        Text("TO").font(.system(.caption))
+                                        Text(to).font(.system(size: 20))
+                                    }
+                                }
+                            }
+                            .frame(width: 150, height: 70.0)
+                            .foregroundColor(.black).opacity(0.4)
+                            
+                        }
+                        DateView(date:$departure,dateString:$departureString)
+                            .frame(height:70.0)
+                            .foregroundColor(.black).opacity(0.4)
+                        
+                        
+                    } label: {
+                        Label("Search flight", systemImage: "airplane.arrival")
+                            .opacity(0.6)
+                    }
+                    .groupBoxStyle(.created)
+                    
+//                    Spacer().frame(height: 20)
+                    
+                    Button{
+                        
+                    } label: {
+                        Text("SEARCH")
+                            .frame(height: 51)
+                            .foregroundColor(.black)
+                            .frame(width:geometry.size.width*0.8)
+                            .background(Color(UIColor.ComponentColor))
+                            .cornerRadius(8.0)
+                    }
+                    .padding(.top, 30)
+                    
+                }
+                .frame(width:geometry.size.width*0.8)
+                
             }
-            .frame(width:UIScreen.main.bounds.width*0.8)
-
-        }
-        //Trailing Closures
-        .sheet(isPresented: $isSearchFromViewShowing) {
-            SearchView(bindingQuery: $from, dismissSearchView: $isSearchFromViewShowing, model: model)
-        }
-        .sheet(isPresented: $isSearchToViewShowing) {
-            SearchView(bindingQuery: $to, dismissSearchView: $isSearchToViewShowing, model: model)
+            //Trailing Closures
+            .sheet(isPresented: $isSearchFromViewShowing) {
+                SearchView(bindingQuery: $from, dismissSearchView: $isSearchFromViewShowing, model: model)
+            }
+            .sheet(isPresented: $isSearchToViewShowing) {
+                SearchView(bindingQuery: $to, dismissSearchView: $isSearchToViewShowing, model: model)
+            }
         }
     }
 }
